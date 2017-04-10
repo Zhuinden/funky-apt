4c4c692753ce59bf027f8da130c0b1f7f184f679
Zhuinden <zhuinden@gmail.com>
2017-04-10 23:23:49 +0200

    added OtherThing

--- src/main/java/com/zhuinden/App.java
+++ src/main/java/com/zhuinden/App.java
@@ -18,7 +18,7 @@ public class App {
         createDirectoryIfDoesNotExist("target/generated-sources");
         com.sun.tools.javac.Main.main(Arrays.asList("-proc:only",
                 "-processor", "com.zhuinden.MyProcessor",
-                currentDir + "/src/main/java/com/zhuinden/App.java", "-s", "target/generated-sources").toArray(new String[6]));
+                currentDir + "/src/main/java/com/zhuinden/App.java", currentDir + "/src/main/java/com/zhuinden/OtherThing.java", "-s", "target/generated-sources").toArray(new String[7]));
     }
 
     private static void createDirectoryIfDoesNotExist(String pathname) {
---
+++ src/main/java/com/zhuinden/OtherThing.java
@@ -0,0 +1,13 @@
+package com.zhuinden;
+
+/**
+ * Created by Zhuinden on 2017.04.10..
+ */
+@Funk
+public class OtherThing {
+    App app;
+
+    OtherThing otherThing;
+
+    public static final String BLAH = "blah";
+}
