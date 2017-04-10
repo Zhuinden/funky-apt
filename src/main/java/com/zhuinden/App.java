fe7fa9f6ba44b3b68d3ebd4ffcd35e432289cff3
zhuinden <zhuinden@gmail.com>
2017-04-10 15:28:11 +0200

    create folder if doesn't exist

--- src/main/java/com/zhuinden/App.java
+++ src/main/java/com/zhuinden/App.java
@@ -1,5 +1,6 @@
 package com.zhuinden;
 
+import java.io.File;
 import java.util.Arrays;
 
 /**
@@ -9,8 +10,17 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello World!");
+        createDirectoryIfDoesNotExist("target");
+        createDirectoryIfDoesNotExist("target/generated-sources");
         com.sun.tools.javac.Main.main(Arrays.asList("-proc:only",
                 "-processor", "com.zhuinden.MyProcessor",
                 "c:/Development/HomeProjects/funky-apt/src/main/java/com/zhuinden/App.java", "-s", "target/generated-sources").toArray(new String[6]));
+    }
+
+    private static void createDirectoryIfDoesNotExist(String pathname) {
+        File targetGenSrcDir = new File(pathname);
+        if(!targetGenSrcDir.exists()) {
+            targetGenSrcDir.mkdir();
+        }
     }
 }
